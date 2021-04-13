f4b6a6f5938abbc7babac0252c4843596481ce94
Vitor Guedes <vittorbassdev@gmail.com>
2021-04-13 13:44:53 -0300

    Coponente Challenge Box estilizado

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,49 @@
+import styles from '../styles/components/ChallengeBox.module.css'
+
+export function ChallengeBox() {
+
+  const hasActiveChallenge = true
+  return (
+    <div className={styles.challengeContainer}>
+      {hasActiveChallenge ? (
+        <div className={styles.challengeActive}>
+
+          <header> Ganhe 400 xp </header>
+
+          <main>
+            <img src="icons/body.svg" alt="" />
+            <strong>Novo Desafio</strong>
+            <p>Levante e faça uma caminha de 3 minutos</p>
+          </main>
+          <footer>
+            <button
+              type="button"
+              className={styles.challengeFailedButton}
+
+            >
+              Falhei
+              </button>
+
+            <button
+              type="button"
+              className={styles.challengeSucceededButton}
+            >
+              Completei
+              </button>
+
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>
+            Finalize um clico para receber desafios
+            <p>
+              <img src="icons/level-up.svg" alt="Level Up" />
+                Avance de level completando  desafios
+            </p>
+          </strong>
+        </div>
+      )}
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head'
+import Head                    from 'next/head'
+import { ChallengeBox }        from '../components/ChallengeBox'
 import { CompletedChallenges } from '../components/CompletedChallenges'
-import { Countdown } from '../components/Countdown'
-import { ExperienceBar } from '../components/ExperienceBar'
-import { Profile } from '../components/Profile'
-import styles from '../styles/pages/Home.module.css'
+import { Countdown }           from '../components/Countdown'
+import { ExperienceBar }       from '../components/ExperienceBar'
+import { Profile }             from '../components/Profile'
+import styles                  from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
@@ -23,6 +24,7 @@ export default function Home() {
         </div>
           
         <div>
+          <ChallengeBox/>
           
         </div>
       </section>
